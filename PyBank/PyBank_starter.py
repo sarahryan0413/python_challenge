b86d1e7ed5e39657d425c5f60eeaca86618361ec
Sarah Ryan <sarahryan0413@gmail.com>
2025-01-09 18:05:27 -0600

    started first lines of code

--- PyBank/PyBank_starter.py
+++ PyBank/PyBank_starter.py
@@ -19,29 +19,33 @@ with open(file_to_load) as financial_data:
     reader = csv.reader(financial_data)
 
     # Skip the header row
-    header = next(reader)
+    header = next(reader) 
 
     # Extract first row to avoid appending to net_change_list
+    first_row = next(reader) 
 
+    #take the value in the first row, second column (profit/losses) - idenitified by first_row[1]
+    prior_profit_losses = int(first_row[1]) 
 
-    # Track the total and net change
-
+    # Track the total month and net change 
+    total_months += 1  #count that first row as 1 month
+    total_net += prior_profit_losses  #add that first row's profit/losses to the total net
 
     # Process each row of data
     for row in reader:
-        print(row)
-        # Track the total
+        #print(row)
+        # Track the total months
+        total_months += 1  #count that row as 1 month
 
-
-        # Track the net change
-
+        # Track the net change -- sum the next row to total_net
+        total_net += prior_profit_losses  #add the profit/losses to the total net
 
         # Calculate the greatest increase in profits (month and amount)
 
 
         # Calculate the greatest decrease in losses (month and amount)
 
-
+print(total_net)
 
 # Calculate the average net change across the months
 
@@ -53,5 +57,5 @@ with open(file_to_load) as financial_data:
 
 
 # Write the results to a text file
-with open(file_to_output, "w") as txt_file:
-    txt_file.write(output)
+#with open(file_to_output, "w") as txt_file:
+    #txt_file.write(output)
